25bb73cd33b4fe43704acae856c790c6fad16ae2
educhin <c.s.arinze@gmail.com>
2020-09-04 07:43:20 -0400

    Add Scores and Test components to prepare for routing

---
+++ frontend_tt/src/components/Scores.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Scores extends React.Component {
+    render() {
+        return <h1>Scores function goes here.</h1>
+    }
+}
+
+export default Scores;
---
+++ frontend_tt/src/components/Test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Test extends React.Component {
+    render() {
+        return <h1>Test function goes here.</h1>
+    }
+}
+
+export default Test;
