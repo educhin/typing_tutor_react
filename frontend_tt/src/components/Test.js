3246399386a6a56dd6163b2312c4e3becc3a0736
educhin <c.s.arinze@gmail.com>
2020-09-07 07:54:02 -0400

    Add p tag to test.js

--- frontend_tt/src/components/Test.js
+++ frontend_tt/src/components/Test.js
@@ -7,7 +7,7 @@ class Test extends React.Component {
                 <h1>Test function goes here.</h1>
                 <p id="quote-display" className="quoteDisplay">Type this text in the text area below</p>
                 <textarea id="quote-display" className="quoteBox" autofocus="true"></textarea>
-
+                <p>WPM: 315 | ACC: 100%</p>
             </div>
         )
     }
