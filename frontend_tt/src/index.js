b7a1e3c47fc5c3e9e702b1afeadf3e467ca5e9f2
educhin <c.s.arinze@gmail.com>
2020-09-07 14:00:13 -0400

    Remove unnecessary comments

--- frontend_tt/src/index.js
+++ frontend_tt/src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import { createStore, applyMiddleware } from 'redux'
 import store from  './app/store'
 import { Provider } from 'react-redux';
-// import thunk from 'redux-thunk';
-// import rootReducer from './reducers/rootReducer';
 import App from './containers/App';
 import './index.css';
-
-// const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
