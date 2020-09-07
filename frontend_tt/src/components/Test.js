3399a1d700598f0f449d910324b4997e7556e9a6
educhin <c.s.arinze@gmail.com>
2020-09-07 10:21:18 -0400

    Add functionality to pull quote from state in Test class. (This is not the final product and store should be moved out of this component. This was only for testing)

--- frontend_tt/src/components/Test.js
+++ frontend_tt/src/components/Test.js
@@ -1,12 +1,35 @@
 import React from 'react';
 
 class Test extends React.Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            quote: []
+        }
+    }
+
+    setQuote = (quote) => {
+        this.setState({
+            quote: quote    
+        })
+    }
+    componentDidMount() {
+        fetch('http://api.quotable.io/random')
+        .then(response => response.json())
+        .then(data => {
+            this.setQuote(data.content)
+        })
+    }
+
+
     render() {
         return (
             <div>
                 <h1>Test function goes here.</h1>
-                <p id="quote-display" className="quoteDisplay">Type this text in the text area below</p>
-                <textarea id="quote-display" className="quoteBox" autofocus="true"></textarea>
+                <p id="quoteDisplay" className="quote-display">{this.state.quote}</p>
+                <textarea id="quoteBox" className="quote-box" autofocus="true"></textarea>
                 <p>WPM: 315 | ACC: 100%</p>
             </div>
         )
