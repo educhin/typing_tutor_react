5eee9630fd43ca616dccaf9f030c4171e907f090
educhin <c.s.arinze@gmail.com>
2020-09-08 07:48:04 -0400

    Clean up unnecessary comments in Scores.js

--- frontend_tt/src/components/scores/Scores.js
+++ frontend_tt/src/components/scores/Scores.js
@@ -1,13 +1,3 @@
-// import React from 'react';
-
-// class Scores extends React.Component {
-//     render() {
-//         return <h1>Scores function goes here.</h1>
-//     }
-// }
-
-// export default Scores;
-
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { selectAllScores } from './scoresSlice'
