eb772733b71021aff0bee145190268814cb176ee
educhin <c.s.arinze@gmail.com>
2020-09-08 11:55:20 -0400

    Add charSpan to test.js and add incorrect/correct class plus styling in test.css to prepare for checking for correctness

--- frontend_tt/src/components/test/Test.js
+++ frontend_tt/src/components/test/Test.js
@@ -29,7 +29,13 @@ export const Test = () =>{
 
     } else if (quoteStatus === 'fulfilled') {
       // content = quote
-      quoteDisplayElement.innerText = quote
+      quoteDisplayElement.innerText = ''
+      quote[0].split('').forEach(c => {
+        const charSpan = document.createElement('span')
+        charSpan.innerText = c
+        quoteDisplayElement.appendChild(charSpan)
+      })
+      // console.log(quote[0])
       quoteInputElement.value = null
     } else if (quoteStatus === 'failed') {
       quoteDisplayElement.innerText = error
@@ -37,16 +43,20 @@ export const Test = () =>{
 
     }
 
+    function handleChange(e) {
+      console.log(e.target.value)
+    }
+
     return (
         <div>
             <h1 className="title">Welcome to the test page!</h1>
             <div className="container">
                 <p id="quoteDisplay" className="quote-display"></p>
-                <textarea id="quoteInput" className="quote-input" autoFocus={true}>Failed to fetch, there</textarea>
+                <textarea id="quoteInput" className="quote-input" onChange={handleChange} autoFocus={true}>Failed to fetch, there</textarea>
                 <div className="stats">
                     <p>WPM: 315 | ACC: 100%</p>
                 </div>
-                {console.log(quote)}
+                {/* {console.log(quote)} */}
             </div>
 
         </div>
