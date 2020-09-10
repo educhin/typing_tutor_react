70c03677c87be5439ab2b79e0091075b8d4a98ce
educhin <c.s.arinze@gmail.com>
2020-09-10 03:41:30 -0400

    Comment out scores component and associated files

--- frontend_tt/src/components/scores/scoresSlice.js
+++ frontend_tt/src/components/scores/scoresSlice.js
@@ -1,29 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit'
+// import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = [
-    {
-        name: 'Ogun',
-        position: 1,
-        wpm: 315,
-        acc: 100
-    },
+// const initialState = [
+//     {
+//         name: 'Ogun',
+//         position: 1,
+//         wpm: 315,
+//         acc: 100
+//     },
 
-    {
-        name: 'Pickle Rick',
-        position: 2,
-        wpm: 200,
-        acc: 98
-    }
-]
+//     {
+//         name: 'Pickle Rick',
+//         position: 2,
+//         wpm: 200,
+//         acc: 98
+//     }
+// ]
 
-export const scoresSlice = createSlice({
-    name: 'scores',
-    initialState,
-    reducers: {
+// export const scoresSlice = createSlice({
+//     name: 'scores',
+//     initialState,
+//     reducers: {
 
-    }
-})
+//     }
+// })
 
-export default scoresSlice.reducer
+// export default scoresSlice.reducer
 
-export const selectAllScores = state => state.scores
+// export const selectAllScores = state => state.scores
